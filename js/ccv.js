c94ee7660860504891e383dedfa72b6eb17fe6fc
Forrest Oliphant <forrest@sembiki.com>
2014-04-02 13:14:08 +0300

    require cascade from face.js

--- js/ccv.js
+++ js/ccv.js
@@ -10,6 +10,9 @@ Redistribution and use in source and binary forms, with or without modification,
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
+var face = require('./face.js')
+
 var ccv = module.exports = exports = {
   pre : function (image) {
     if (image.tagName.toLowerCase() == "img") {
@@ -105,6 +108,10 @@ var ccv = module.exports = exports = {
   detect_objects : function (canvas, cascade, interval, min_neighbors) {
     canvas = ccv.pre(canvas);
     if (!canvas) { return false; }
+
+    if (cascade==='face') {
+      cascade = face;
+    }
 
     var scale = Math.pow(2, 1 / (interval + 1));
     var next = interval + 1;
